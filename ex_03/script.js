05bc1ffb2c76c198b7cb67bc1a5a187179c12cf2
CaioGTF <105945358+CaioGTF@users.noreply.github.com>
2024-09-11 13:52:07 +0000

    script

---
+++ ex_03/script.js
@@ -0,0 +1,20 @@
+const inputNum = document.getElementById("inputNum");
+const inputVal = document.getElementById("inputVal");
+const inputMedia = document.getElementById("inputMedia");
+const btAdicionar = document.getElementById("btAdicionar");
+const inputSoma = document.getElementById("inputSoma");
+const inpuQtd = document.getElementById("inputQtd");
+valores = [];
+let soma = 0;
+let qtd = 0;
+btAdicionar.onclick = () => {
+    let num = parseInt(inputNum.value);
+    valores.push(num);
+    inputVal.value = valores.join(", "); 
+    ++qtd;
+    soma += num;
+    inputSoma.value = soma;
+    inputQtd.value = qtd;
+    inputMedia.value = soma / qtd;
+    
+}
