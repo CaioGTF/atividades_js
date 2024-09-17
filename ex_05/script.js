c35113e463c391cd2e3dd8cd09cb678684db41ad
CaioGTF <105945358+CaioGTF@users.noreply.github.com>
2024-09-17 13:22:32 +0000

    jogo da forca

--- ex_05/script.js
+++ ex_05/script.js
@@ -26,7 +26,7 @@ let tentativasRestantes;
 let numeroErros;
 
 function iniciarJogo() {
-    palavraEscolhida = listaPalavras[Math.floor(Math.random()*listaPalavras.length)];
+    palavraEscolhida = listaPalavras[Math.floor(Math.random() * listaPalavras.length)];
 
     exibicaoPalavra = Array(palavraEscolhida.length).fill('_');
     console.log(exibicaoPalavra);
@@ -39,4 +39,52 @@ function iniciarJogo() {
 
     atualizarExibicao();
 }
+
+function atualizarExibicao() {
+    document.getElementById("exibicao-palavra").innerText = exibicaoPalavra.join(' ');
+    document.getElementById("letras_chutadas").innerText = `${letrasChutadas.join(', ')}`;
+
+    document.getElementById("imagem").src = `imagens_forca/forca${numeroErros}.png`;
+
+    if(tentativasRestantes === 0) {
+        encerrarJogo('Você morreu!!');
+    } else if(!exibicaoPalavra.includes('_')) {
+        encerrarJogo('Parabéns, você venceu!!');
+    }
+}
+function chutarLetra() {
+    const entradaLetra = document.getElementById('entrada_letra');
+    const letra = entradaLetra.value.toLowerCase();
+    if(!letra.match(/[a-zà-ùç]/i)) {
+        alert('Por favor, insira uma letra válida!');
+    }
+
+    if(letrasChutadas.includes(letra)) {
+        alert('Você já tentou esta letra. Tente outra');
+        return;
+    }
+    letrasChutadas.push(letra);
+
+    if(palavraEscolhida.includes(letra)) {
+        for (let i=0; i< palavraEscolhida.length; i++) {
+            if(palavraEscolhida[i] === letra) {
+                exibicaoPalavra[i] = letra;
+            } else {
+                tentativasRestantes--;
+                numeroErros++;
+            }
+        }
+    }
+    entradaLetra.value = '';
+    atualizarExibicao();
+}
+
+
+function encerrarJogo(mensagem) {
+    document.getElementById('entrada-letra').disabled = true;
+    document.getElementById('mensagem').innerText = mensagem;
+
+    document.getElementById('recomecar').style.display = 'block';
+}
+ 
 window.load = iniciarJogo();
