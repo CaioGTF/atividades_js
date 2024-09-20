a7f6772ff22e871fbee0716214413533d4170393
CaioGTF <105945358+CaioGTF@users.noreply.github.com>
2024-09-20 22:31:32 +0000

    fetch

---
+++ ex_06/script.js
@@ -0,0 +1,14 @@
+const inputID = document.getElementById("inputID");
+const btFetch = document.getElementById("btFetch");
+
+const handlebtFetchClick = () => {
+    const id = inputID.value.trim();
+    if (!id) {
+        alert("Precisa digitar um ID");
+        return;
+    }
+    fetch(`https://swapi.dev/api/people/${id}`).then((response)) => {
+        console.log
+        }
+};
+
