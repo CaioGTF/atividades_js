376deb65298d04686880d3ee9bd4dee446f3b058
CaioGTF <105945358+CaioGTF@users.noreply.github.com>
2024-09-25 17:30:40 +0000

    fetch

--- ex_06/script.js
+++ ex_06/script.js
@@ -1,36 +1,14 @@
-const inputID = document.getElementById("inputID");
-const btFetch = document.getElementById("btFetch");
-const prePersonagem = document.getElementById("prePersonagem");
+const repositories = document.querySelector('.content-main');
 
-const handleBtFetchClick = () => {
-  const id = inputID.value.trim();
-  if (!id) {
-    alert("Precisa digitar o ID!");
-    return;
-  }
+function getApiGithub() {
+  fetch(`http://api.anidb.net:9001/httpapi?request=anime`)
+  .then(async res => {
+    if(!res.ok) {
+      throw new Error(res.status);
+    }
+    let data = await res.json();
+    console.log(data);
+  })
+}
 
-  fetch(`https://swapi.dev/api/people/${id}`)
-    .then((response) => {
-      console.log(response);
-      if (response.ok) {
-        console.log("Sucesso!");
-      } else {
-        console.log("Erro: ", response.status, response.statusText);
-        alert("Erro ao acessar o servidor - ID inválido?");
-        throw new Error("Erro encontrado: " + response.status);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log("data", data);
-      // prePersonagem.innerHTML = JSON.stringify(data);
-      prePersonagem.innerHTML = `${data.name} - ${data.birth_year}`;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
-
-btFetch.onclick = handleBtFetchClick;
-
-window.onload = () => fetchList(`http://api.anidb.net:9001/httpapi?request=anime`);
+getApiGithub()
