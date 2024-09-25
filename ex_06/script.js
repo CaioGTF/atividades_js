ec1c7a45c479581142e5111c0b2397f627cc2de2
CaioGTF <105945358+CaioGTF@users.noreply.github.com>
2024-09-25 18:26:06 +0000

    fetch

--- ex_06/script.js
+++ ex_06/script.js
@@ -1,7 +1,7 @@
 const repositories = document.querySelector('.content-main');
 
 function getApiGithub() {
-  fetch(`https://api.thecatapi.com/v1/images/search?limit=10`)
+  fetch(`https://api.github.com/users/nickname/repos`)
   .then(async res => {
     if(!res.ok) {
       throw new Error(res.status);
@@ -10,8 +10,18 @@ function getApiGithub() {
     data.map( item => {
       let project = document.createElement('div');
 
-      project.innerHTML = `Teste`
-
+      project.innerHTML = `
+      <div class="project">
+            <div>
+                <h4 class="title">${item.name}</h4>
+                <span class="date-create">${ item.created_at}</span>
+            </div>
+            <div>
+                <a href="${ item.html_url}">${ item.html_url}</a>
+                <span class="language"> <span class="circle"></span>${ item.language}</span>
+            </div>
+        </div>
+      `
       repositories.appendChild(project);
     })
   })
