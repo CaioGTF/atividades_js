876113c192e09521e88ae1448886a58b060989db
CaioGTF <105945358+CaioGTF@users.noreply.github.com>
2024-10-04 23:04:11 +0000

    commit

--- ex_07/script.js
+++ ex_07/script.js
@@ -1,42 +1,120 @@
-const olPersonagens = document.getElementById("olPersonagens");
-const btPrevious = document.getElementById("btPrevious");
-const btNext = document.getElementById("btNext");
+const olTarefas = document.getElementById("olTarefas");
+const inputDescricao = document.getElementById("inputDescricao");
+const btAdicionar = document.getElementById("btAdicionar");
 
-const createList = (url, data) => {
-  const personagens = data.results;
-  const page = Number(url.split("=")[1]);
-  olPersonagens.innerHTML = "";
-  olPersonagens.start = `${(page - 1) * 10 + 1}`;
-  personagens.forEach((personagem) => {
-    const text = document.createTextNode(
-      `${personagem.name} - ${personagem.birth_year}`
-    );
+const baseURL = "https://parseapi.back4app.com/classes/Tarefa";
+const headers = {
+  "X-Parse-Application-Id": "4cRNaPFAu9tGO59OrBIJpH4v6qeHUUbtReuVmjP7",
+  "X-Parse-REST-API-Key": "x3OZm4ZBkTNLy3jJUc9JImiuAi6XK4ih7zfKOVwk",
+};
+const headersJson = {
+  ...headers,
+  "Content-Type": "application/json",
+};
+
+const createList = (data) => {
+  olTarefas.innerHTML = "";
+  const tarefas = data.results;
+  tarefas.forEach((tarefa) => {
+    const text = document.createTextNode(`${tarefa.descricao} `);
     const li = document.createElement("li");
     li.appendChild(text);
-    olPersonagens.appendChild(li);
+    const cb = document.createElement("input");
+    cb.type = "checkbox";
+    cb.checked = tarefa.concluida;
+    cb.onchange = () => handleCheckboxClick(cb, tarefa);
+    li.appendChild(cb);
+    const bt = document.createElement("button");
+    bt.innerHTML = "x";
+    bt.onclick = () => handleBtRemoverClick(bt, tarefa);
+    li.appendChild(bt);
+    olTarefas.appendChild(li);
   });
-  btPrevious.disabled = data.previous == null;
-  btPrevious.onclick = () => fetchList(data.previous);
-  btNext.disabled = data.next == null;
-  btNext.onclick = () => fetchList(data.next);
 };
 
-const fetchList = (url) => {
-  fetch(url)
-    .then((response) => {
-      console.log(response);
-      if (!response.ok) {
-        alert("Erro ao acessar o servidor: " + response.status);
-        throw new Error("Erro encontrado: " + response.status);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      createList(url, data);
-    })
-    .catch((error) => {
-      console.log(error);
+const handleCheckboxClick = async (cb, tarefa) => {
+  try {
+    cb.disabled = true;
+    const response = await fetch(`${baseURL}/${tarefa.objectId}`, {
+      method: "PUT",
+      headers: headersJson,
+      body: JSON.stringify({ concluida: !tarefa.concluida }),
     });
+    cb.disabled = false;
+    console.log(response);
+    if (!response.ok) {
+      cb.checked = !cb.checked;
+      alert("Erro ao acessar o servidor: " + response.status);
+      throw new Error("Erro encontrado: " + response.status);
+    }
+  } catch (error) {
+    cb.checked = !cb.checked;
+    console.log(error);
+  }
 };
 
-window.onload = () => fetchList(`https://swapi.dev/api/people/?page=1`);
+const handleBtRemoverClick = async (bt, tarefa) => {
+  try {
+    bt.disabled = true;
+    const response = await fetch(`${baseURL}/${tarefa.objectId}`, {
+      method: "DELETE",
+      headers: headers,
+    });
+    bt.disabled = false;
+    console.log(response);
+    if (!response.ok) {
+      alert("Erro ao acessar o servidor: " + response.status);
+      throw new Error("Erro encontrado: " + response.status);
+    }
+    getTarefas();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getTarefas = async () => {
+  try {
+    const response = await fetch(baseURL, {
+      method: "GET",
+      headers: headers,
+    });
+    console.log(response);
+    if (!response.ok) {
+      alert("Erro ao acessar o servidor: " + response.status);
+      throw new Error("Erro encontrado: " + response.status);
+    }
+    const data = await response.json();
+    createList(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const handleBtAdicionarClick = async () => {
+  const descricao = inputDescricao.value.trim();
+  if (!descricao) {
+    alert("Necessário adicionar uma descrição para criar a tarefa!");
+    inputDescricao.focus();
+    return;
+  }
+  try {
+    const response = await fetch(baseURL, {
+      method: "POST",
+      headers: headersJson,
+      body: JSON.stringify({ descricao: descricao }),
+    });
+    console.log(response);
+    if (!response.ok) {
+      alert("Erro ao acessar o servidor: " + response.status);
+      throw new Error("Erro encontrado: " + response.status);
+    }
+    inputDescricao.value = "";
+    inputDescricao.focus();
+    getTarefas();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+window.onload = getTarefas;
+btAdicionar.onclick = handleBtAdicionarClick;
